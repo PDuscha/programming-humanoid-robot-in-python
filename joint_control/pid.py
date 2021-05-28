8b29649312ad45b801ddb38cc5bcc6a3b3ae5ed8
Paul Duscha <paul.duscha@campus.tu-berlin.de>
2021-05-28 13:44:32 +0200

    pid & parameters done

--- joint_control/pid.py
+++ joint_control/pid.py
@@ -35,9 +35,9 @@ class PIDController(object):
         self.e2 = np.zeros(size)
         # ADJUST PARAMETERS BELOW
         delay = 0
-        self.Kp = 0
-        self.Ki = 0
-        self.Kd = 0
+        self.Kp = 20
+        self.Ki = 0.7
+        self.Kd = 0.1
         self.y = deque(np.zeros(size), maxlen=delay + 1)
 
     def set_delay(self, delay):
@@ -53,7 +53,14 @@ class PIDController(object):
         @return control signal
         '''
         # YOUR CODE HERE
-
+        error = target - sensor
+        self.u = self.u + (self.Kp+self.Ki*self.dt+(self.Kd/self.dt))*error - (self.Kp+((2*self.Kd)/self.dt))*self.e1 + (self.Kd/self.dt)*self.e2
+        self.e2 = self.e1
+        self.e1 = error
+        y = self.y.popleft()
+        speed = ((y - sensor) + (self.u - sensor))/(2*self.dt)
+        prediction = self.u + (speed*self.dt)
+        self.y.append(prediction)
         return self.u
 
 
